007dda22c8af3c242f2ecd0210b8de9563464e29
Ron Lusk <rlusk@chariotsolutions.com>
2018-10-08 10:38:53 -0400

    Make demo more realistic, add some timings, update README

--- README.md
+++ README.md
@@ -6,11 +6,57 @@ Provide indexing on data in a map
 
 ### With map values that are maps
 
-    ;; Example here
+    (ns indexed-map.demo
+      "Demo use of indexed-map"
+      (:require [com.chariotsolutions.indexed-map :as im :refer [indexed-map indexed-get]]))
+    (defn sample-code
+      []
+      (let [database (-> (indexed-map [:first :last])
+                         (assoc :id01 {:first "Jordan" :last "Marsh"})
+                         (assoc :id02 {:first "Hunter" :last "Forest"})
+                         (assoc :id02 {:first "Jordan" :last "Morgan"}))]
+        (indexed-get database :first "Jordan")))
+    (sample-code)
+    ;;==> [{:first "Jordan", :last "Morgan"} {:first "Jordan", :last "Marsh"}]
 
 ### With map values that are atoms
 
-    ;; Example here
+    ;; Not yet implemented
+    
+## Performance
+
+With 2,000,001 records in an `IndexedMap`, with *one* record with
+field `:c` containing `"QQ"` ("RAW" indicates a normal Clojure map
+containing the same data.)
+
+    Searching 'Sequential for 'QQ'' in 2000001 record(s)
+    "Elapsed time: 1185.022334 msecs"
+    Found 1 record(s)
+    
+    Searching 'Sequential RAW for 'QQ'' in 2000001 record(s)
+    "Elapsed time: 1131.917573 msecs"
+    Found 1 record(s)
+    
+    Searching 'Indexed for 'QQ'' in 2000001 record(s)
+    "Elapsed time: 0.036315 msecs"
+    Found 1 record(s)
+
+Searching the same two (indexed and Clojure) databases for a value
+that appears 40,000 times:
+
+    Searching 'Sequential for 'LO'' in 2000001 record(s)
+    "Elapsed time: 1728.361863 msecs"
+    Found 400000 record(s)
+
+    Searching 'Sequential RAW for 'LO'' in 2000001 record(s)
+    "Elapsed time: 1514.728234 msecs"
+    Found 400000 record(s)
+
+    Searching 'Indexed for 'LO'' in 2000001 record(s)
+    "Elapsed time: 563.877368 msecs"
+    Found 400000 record(s)
+
+(Times on a MacBook Pro.)
 
 ## License
 
--- src/com/chariotsolutions/indexed_map/demo.clj
+++ src/com/chariotsolutions/indexed_map/demo.clj
@@ -7,17 +7,63 @@
   [n]
   (let [red-fn (fn [im [a b c]]
                  (assoc im (str a) {:a a :b b :c c}))]
-    (reduce red-fn (indexed-map [:a :b :c])
-            (map #(vector %1 %2 %3) (range n) (cycle (range 7))
-                 (cycle ["HE" "EL" "LL" "LO" "OH"])))))
+    (-> (reduce red-fn (indexed-map [:a :b :c])
+             (map #(vector %1 %2 %3) (range n) (cycle (range 7))
+                  (cycle ["HE" "EL" "LL" "LO" "OH"])))
+        ;; Add one unique record
+        (assoc :only-keyword {:a 9999 :b 8 :c "QQ"}))))
 
+
+(defn run-and-report
+  "Run a `search-fn` through a `source` map for records with a given
+  value in a particular field. Print `details` and timing. Return the
+  vector containing search results."
+  [details source search-fn]
+  (printf "Searching '%s' in %d record(s)\n" details (count source))
+  (let [result (time (doall (into #{} (search-fn source))))]
+    (printf "\tFound %d record(s)\n" (count result))
+    result))
 
 (defn try-it-out
+  "Search an `indexed-map` and a `hash-map` for unique records with a
+  value in some field, or for many duplicates, displaying elapsed
+  times using various methods of searching."
+  ([]
+   (try-it-out 20000))
+  ([number-to-load]
+   (let [test-map (load-a-lot number-to-load)
+         clj-map (into {} (seq test-map))
+         sequential-search-fn (fn [kwd val]
+                                (fn [database]
+                                  (filter #(#{val} (kwd %))
+                                          (vals database))))
+         sequential-QQ (sequential-search-fn :c "QQ")
+         sequential-LO (sequential-search-fn :c "LO")
+         sequential-single (run-and-report "Sequential for 'QQ'"
+                                           test-map
+                                           sequential-QQ)
+         sequential-single-clj (run-and-report "Sequential RAW for 'QQ'" clj-map sequential-QQ)
+         indexed-single (run-and-report "Indexed for 'QQ'" test-map
+                                        (fn [database]
+                                          (indexed-get database :c "QQ")))
+         sequential-many (run-and-report "Sequential for 'LO'"
+                                         test-map
+                                         sequential-LO)
+         sequential-many-clj (run-and-report "Sequential RAW for 'LO'"
+                                         clj-map
+                                         sequential-LO)
+
+         indexed-many (run-and-report "Indexed for 'LO'" test-map
+                                      (fn [database]
+                                        (indexed-get database :c "LO")))]
+     (println "Single equal? " (= sequential-single indexed-single sequential-single-clj))
+     (println "Many equal? " (= sequential-many indexed-many sequential-many-clj)))))
+
+
+(defn sample-code
   []
-  (let [test-map (load-a-lot 50000)
-        sequential-results
-        (time (doall (into #{} (filter #(#{"OH"} (:c %))
-                                       (vals test-map)))))
-        indexed-results
-        (time (doall (into #{} (indexed-get test-map :c "OH"))))]
-    (println "Equal? " (= sequential-results indexed-results))))
+  (let [database (-> (indexed-map [:first :last])
+                     (assoc :id01 {:first "Jordan" :last "Marsh"})
+                     (assoc :id02 {:first "Hunter" :last "Forest"})
+                     (assoc :id02 {:first "Jordan" :last "Morgan"}))]
+    (indexed-get database :first "Jordan")))
