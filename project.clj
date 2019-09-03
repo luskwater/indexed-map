b7da4bb1e8970a84d033c8ef2f16352aefc4e6b5
Ron Lusk <rlusk@chariotsolutions.com>
2019-09-03 11:05:10 -0400

    Add Travis CI support; update project.clj
    Add `.travis.yml` to learn about *Travis CI*
    
    Update `:description` and `:url` tags in `project.clj`

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.chariotsolutions/indexed-map "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Provide indexing on data in a map"
+  :url "https://github.com/luskwater/indexed-map"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
