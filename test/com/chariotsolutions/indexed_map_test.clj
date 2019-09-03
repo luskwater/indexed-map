86c456505dcdeff53a2b5efba7a6870c8437d4fe
Ron Lusk <rlusk@chariotsolutions.com>
2019-09-03 14:51:00 -0400

    Test `reduce-kv` support

--- test/com/chariotsolutions/indexed_map_test.clj
+++ test/com/chariotsolutions/indexed_map_test.clj
@@ -7,3 +7,13 @@
     (let [uut (-> (indexed-map)
                   (assoc :foo 12))]
       (is (= 12 (:foo uut))))))
+
+(deftest reduce-kv-test
+  (testing "reduce-kv interface"
+    (let [uut (-> (indexed-map)
+                  (assoc :foo 12))]
+      (reduce-kv (fn [init k v]
+                   (is (= 147 init))
+                   (is (= :foo k))
+                   (is (= 12 v)))
+                 147 uut))))
